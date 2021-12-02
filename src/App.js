d23e3be41bf0c7faaebd320107520408aaf4a96f
mdrazu722901 <mdrajuhossen722901@gmail.com>
2021-12-02 09:33:55 -0800

    auth-recap-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebase.config';
+import { getAuth, signInWithPopup, FacebookAuthProvider, GithubAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+const app = initializeApp(firebaseConfig);
 function App() {
+  const [user, setUser] = useState({})
+  console.log(user);
+  const fbProvider = new FacebookAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
+  const fdAuthantication = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, fbProvider)
+      .then((result) => {
+        const user = result.user;
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+        console.log(result);
+        setUser(result._tokenResponse);
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        // ...
+      });
+  };
+  const GithubAuthentication = () => {
+    console.log("github authentication check, confrom")
+    const auth = getAuth();
+    signInWithPopup(auth, gitHubProvider)
+      .then((result) => {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error);
+        console.log(credential);
+        // ...
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>this is my practice project.</h1>
+      <button onClick={fdAuthantication}>Facebook authantication</button>
+      <br />
+      <button onClick={GithubAuthentication}>GitHub Authentication</button>
+      <h1>Full Name: {user.fullName}</h1>
+      <h3>Email:{user.email}</h3>
+      <img src={user.photoUrl} alt="" />
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJ-v0wH5ThKxhGLP49YApS5jbnVxmafgg",
+    authDomain: "auth-recap-2r.firebaseapp.com",
+    projectId: "auth-recap-2r",
+    storageBucket: "auth-recap-2r.appspot.com",
+    messagingSenderId: "5940085663",
+    appId: "1:5940085663:web:0d3135cff27bf2492bf3aa"
+  };
+  export default firebaseConfig;
